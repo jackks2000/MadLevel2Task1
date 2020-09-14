9bdc53c15fd915677dacd9c9b90d33ac11dfb046
Jack <Jack.klein.schiphorst@hva.nl>
2020-09-14 10:33:25 +0200

    working version

---
+++ app/src/main/java/com/example/myapplication/PlaceAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ItemPlaceBinding
+
+class PlaceAdapter (private val places: List<Place>) :
+RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) :
+            ViewHolder{
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(places[position])
+    }
+
+    inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView){
+            val binding = ItemPlaceBinding.bind(itemView)
+
+        fun databind(place:Place){
+            binding.tvPlace.text = place.name
+            binding.ivPlace.setImageResource(place.imageResId)
+        }
+
+
+    }
+
+}
